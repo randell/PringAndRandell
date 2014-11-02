3a22bc5e03bc63bfecf05f5cbd8c3dad5f8e458e
randell <randell.benavidez@readingroom.com.au>
2014-11-02 22:18:15 +1100

    Added condition width depending on screen size; Added sample RSVP page using Google Docs

---
+++ rsvp/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pring &amp; Randell - April 2015">
+    <meta name="author" content="Pring Octaviano and Randell Benavidez">
+    
+    <title>Pring &amp; Randell - April 2015</title>
+
+    <!-- Bootstrap -->
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    
+    <!-- Custom fonts for this template -->
+    <link href='http://fonts.googleapis.com/css?family=Lovers+Quarrel' rel='stylesheet' type='text/css'>
+    <link href="/assets/css/jquery.fullPage.css" rel="stylesheet">
+    <link href="/assets/css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+     <iframe src="https://docs.google.com/forms/d/1Xhu8Eut9FQah6iWgoZC2ZcjVda8cZ7hKyzbk5r9Btqk/viewform?embedded=true">Loading...</iframe>
+  </body>
+</html>
